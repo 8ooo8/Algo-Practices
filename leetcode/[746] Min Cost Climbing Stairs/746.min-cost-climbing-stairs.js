c2bff94fb8383588e03f904e2b791c468b8defce
8ooo8 <ben.cky.workspace@gmail.com>
2021-06-09 03:19:41 +0800

    Q746 DP

--- leetcode/[746] Min Cost Climbing Stairs/746.min-cost-climbing-stairs.js
+++ leetcode/[746] Min Cost Climbing Stairs/746.min-cost-climbing-stairs.js
@@ -10,16 +10,11 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    /*
-     * According to my understanding, the test cases and results provided by leetcode has a minor problem.
-     * For instance, according to leetcode, when the input cost is [10,15,20], the output is 15. 
-     * However, the min cost should be 10 (start with step 0 and the climb to the top), according to my understanding.
-     */
-    if (cost.length === 2) return 0;
-    if (cost.length === 3) return Math.min(cost[0], cost[1]);
-    return cost.slice(2).reduce((minCost, c) => {
+    if (cost.length === 2) return Math.min(cost[0], cost[1]);
+    const last2StepsMinCost = cost.slice(2).reduce((minCost, c) => {
         [minCost.current, minCost.pre] = [c + Math.min(minCost.current, minCost.pre), minCost.current];
         return minCost;
-    }, {current: cost[1], pre: cost[0]}).current - cost[cost.length - 1];
+    }, {current: cost[1], pre: cost[0]});
+    return Math.min(last2StepsMinCost.current, last2StepsMinCost.pre);
 };
 // @lc code=end
